367b170aa6c0db08a3f9bc4ab6e15fd29b9d6405
EC2 Default User <ec2-user@ip-172-31-15-116.ec2.internal>
2021-01-30 12:35:27 +0000

    user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
     
-  root to: 'tasks#index'
   
-  resources :tasks
+  root to: 'tasks#index'
+
+  get 'signup', to: 'users#new'
+  resources :tasks, :users, only: [:index, :show, :new, :create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_125950) do
+ActiveRecord::Schema.define(version: 2021_01_30_121553) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "users_id"
+    t.index ["users_id"], name: "index_tasks_on_users_id"
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
